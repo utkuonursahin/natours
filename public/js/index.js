66065f7c74598bc2e80ddae24cc5662b351c3d8c
utkuonursahin <sahinutkuonur@gmail.com>
2022-08-17 15:22:56 +0300

    map open

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,7 @@ const bookBtn = document.getElementById('book-tour')
 // DELEGATION
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations)
-  // displayMap(locations)
+  displayMap(locations)
 }
 if (loginForm) {
   loginForm.addEventListener('submit', e => {
