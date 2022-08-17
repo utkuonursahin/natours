7104dd3c5b91850d1cc248ab394a3261171241ac
utkuonursahin <sahinutkuonur@gmail.com>
2022-08-17 17:48:36 +0300

    stripe bug fix 4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -40,12 +40,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0])
 // })
 
-const createBookingCheckout = async session => {
-  const tour = session.client_reference_id;
+const createBookingCheckout = async (session) => {
+  const tour = await Tour.findById(session.client_reference_id);
   const user = (await User.findOne({email: session.customer_email})).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].price_data.unit_amount / 100;
   await Booking.create({tour, user, price});
-};
+}
 
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
